47a9e2abd5d42c858f7730c3e019360ff3c0cd06
Kar Man Tan <carmen-tan@hotmail.com>
2026-08-20 18:16:53 +0200

    version in ssrn

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-cd "/Users/tankarman/Library/CloudStorage/OneDrive-Personal/PhD/Thesis/Chapter 1/post_jri"
-rm -f 160526_ch1_sat.spl
-/Library/TeX/texbin/pdflatex 160526_ch1_sat.tex
-/Library/TeX/texbin/biber 160526_ch1_sat
-/Library/TeX/texbin/pdflatex 160526_ch1_sat.tex
-/Library/TeX/texbin/pdflatex 160526_ch1_sat.tex
-/Library/TeX/texbin/pdflatex 160526_ch1_sat.tex
+#!/bin/sh
+set -eu
+
+cd "$(dirname "$0")"
+exec latexmk -pdf -interaction=nonstopmode -synctex=1 -file-line-error main.tex
